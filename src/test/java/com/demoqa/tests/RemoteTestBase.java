013b463df2e13e18f758c3129175b8dcf08cb696
natalya.dolmatova <dolmatova-nata@mail.ru>
2023-07-02 19:45:37 +0300

    add allure+jenkins

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -19,6 +19,7 @@ public class RemoteTestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
