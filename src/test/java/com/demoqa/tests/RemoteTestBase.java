3cbadb101f4fd72bcb9bf0450532152b5de1aaa5
natalya.dolmatova <dolmatova-nata@mail.ru>
2023-07-10 09:36:38 +0300

    refactoring after codereview

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -21,7 +21,7 @@ public class RemoteTestBase {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = System.getProperty("seleniod", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
 
